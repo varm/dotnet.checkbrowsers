d7fc0e846dee2fcf62f38fa9ab72adac8ccf4ff6
zerow <htmlhao@gmail.com>
2022-09-02 16:38:22 +0800

    First commit

---
+++ BrowserEntity.cs
@@ -0,0 +1,13 @@
+/*
+ * Created on Fri Sep 02 2022
+ *
+ * Copyright (c) 2022 - Zerow
+ */
+
+public class BrowserEntity
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public string IconPath { get; set; }
+    public string Version { get; set; }
+}
